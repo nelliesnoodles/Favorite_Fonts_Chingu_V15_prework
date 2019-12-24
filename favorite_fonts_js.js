f69b599853d834ece5d80afd50d2998504e03886
Nellie Tobey <35578867+nelliesnoodles@users.noreply.github.com>
2019-12-23 21:29:09 -0500

    Search fonts added and working

--- favorite_fonts_js.js
+++ favorite_fonts_js.js
@@ -14,6 +14,7 @@ let type_something;
 let reset;
 let main;  //<-- not in use yet
 let body;
+let font_deck;
 
 const Http = new XMLHttpRequest();
 const url='https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCM8prGHcP7hRrejX-WrHzBxx6H-5IPzpQ';
@@ -48,6 +49,7 @@ function set_DOM() {
     reset = document.getElementById("reset");
     //main = document.getElementById("main");
     body = document.getElementById("body");
+    font_deck = document.getElementById("FontDeck")
 }
 
 
@@ -163,7 +165,7 @@ function font_size_select(font){
 
 }
 
-
+// NOT IN USE YET ------
 function remove_400_fonts(){
   // These fonts fail to load See README.md
   var molle = "Molle"
@@ -173,49 +175,297 @@ function remove_400_fonts(){
   var coda = "Coda Caption"
 
 }
+// --------------- END
 
-function append_font_names(){
+
+
+// Make a smaller list with first letter from Search
+function search_for(astring, alist) {
+    newlist = [];
+    size = alist.length;
+
+    i = 0
+
+    for (j = 0; j < size; j++) {
+        max = astring.length
+        word = alist[j];
+        first_in_word = word.charAt(i)
+        first_in_string = astring.charAt(i)
+        first_in_string = first_in_string.toUpperCase()
+        //console.log("first in string =", first_in_string)
+        if (first_in_word == first_in_string) {
+            newlist.push(word)
+        }
+    };
+
+    if (newlist.length > 0) {
+        //console.log(newlist)
+        //set this list that matches the first letter as the list we will
+        //search through into the window under a global SEARCHLIST
+        //If our list in a search is huge, we don't want to be searching through
+        //it every time a letter changes... lets make it smaller, and mutable.
+        window.SEARCHLIST = newlist
+        //console.log(window.SEARCHLIST)
+    }
+    else {
+        console.log("No match found")
+    }
+
+}
+
+//------- END
+//---- revert the tuple list to just font names --
+function revert_names(sortedlist){
+  newlist = []
+  for(item=0; item < sortedlist.length; item++){
+    //font = sortedlist[i][0]
+    tuple = sortedlist[item]
+    //console.log[tuple]
+    font = tuple[0]
+    newlist.push(font)
+  }
+  return newlist
+}
+
+//  Sorting function for search -----
+
+function sort_tuples(tuple_array) {
+    /* from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+     * var numbers = [4, 2, 5, 1, 3];
+       numbers.sort(function(a, b) {
+       return a - b;
+        });
+     */
+    tuple_array.sort(function (a, b) {
+        a = a[1]
+        b = b[1]
+        return a - b
+    })
+
+    //reverse it, because the numbers are sorted by string code, not numeric value
+    reversed = tuple_array.reverse()
+    newlist = revert_names(reversed)
+    return newlist
+}
+
+//------  END SORTING FUNCTION
+
+//---- Make tuples of value matches to search value---
+function search_more(search_value) {
+    //use the stored window.SEARCHLIST in our search for matching/similar strings
+    var search_list = window.SEARCHLIST
+    var search_list_size = search_list.length
+    //console.log("***************")
+    //console.log(search_list)
+    //console.log("***************")
+    var percentage_ranked_values = []
+
+    if (search_list_size > 1 && search_list_size > 0) {
+        //grab the astring and see what percentage of letters match the search.value
+        //we'll set up the search keydown to only run this search if the value length is greater than 1.
+        //percent match will be matched-letters divided by number of letters in the user string
+        //the astring and search value need to be compared.
+
+        for (i = 0; i < search_list_size; i++) {
+            length_of_user_word = search_value.length
+            word = search_list[i]
+            total = 0
+            //console.log("loop number: ", i)
+            //console.log(search_list_size)
+
+            if (word.length >= search_value.length) {
+                for (n = 0; n <= length_of_user_word; n++) {
+                    x = word.charAt(n)
+                    y = search_value.charAt(n)
+                    if (x == y) {
+                        total += 1;
+                    }
+                };
+            }
+            // change the second for loop to reflect that the search value is longer than the word from the list
+            // an index error will happen if we're trying to get a charAt() that has a greater index than the string has characters.
+            else {
+                length_of_search_value = search_value.length
+                for (b = 0; b <= word.length; b++) {
+                    x = word.charAt(b)
+                    y = search_value.charAt(b)
+                    if (x == y) {
+                        total += 1;
+                    }
+                };
+            }
+            if (total > 0) {
+                percentage = parseInt(total) / parseInt(search_value.length)
+                fixed_percentage = percentage.toFixed(3)
+                tuple = [word, fixed_percentage]
+                percentage_ranked_values.push(tuple)
+                //console.log(tuple)
+            }
+            else {
+                percentage = parseInt(0)
+                tuple = [word, percentage]
+                percentage_ranked_values.push(tuple)
+            }
+
+        }
+
+    };
+
+    //console.log("The created tuple list is:")
+    //console.log(percentage_ranked_values)
+    sorted = sort_tuples(percentage_ranked_values)
+    //console.log(sorted)
+    return sorted
+
+
+
+}
+
+//  ------------------ END ------------
+
+// Create a keydown function for when search values are entered---
+function call_keydown_search() {
+      //in html, an onkeyup="call_keydown" will trigger this function.
+      element = document.getElementById("search")
+      newstring = element.value
+      //Retrieve the first letter match list
+      //This is only to match the first letter
+      element_length = newstring.length
+      search_array = font_names
+
+      if (element_length == 1 && element_length != 0) {
+          search_for(newstring, search_array)
+          current = 0
+          //console.log("first letter is entered, setting the window.SEARCHLIST")
+
+      }
+      else if (element_length > 1) {
+          //Check that there is a list stored in the window:
+          if (window.SEARCHLIST) {
+              //blog notes:
+              //lets make sure this bit is working with a console.log()
+              //I went back into our search_for()  and removed the console.log of the newlist.
+              // x = window.SEARCHLIST
+              //console.log("window.SEARCHLIST exists")
+              //console.log(x)
+              sorted_list = search_more(newstring)
+              font_deck.innerHTML = ''
+              append_font_names(search=true, searchlist=sorted_list)
+              create_cards(search=true, searchlist=sorted_list)
+
+          }
+
+      }
+      else {
+        //blog notes:
+          // our element_length is not 1, or greater than 1, so it is less than 1.  We want
+          //the window.SEARCHLIST emptied so if they are starting a new search string, we have a fresh list.
+          window.SEARCHLIST = [];
+      }
+
+
+  }
+//------------------------END -------------------------
+//----- check if link exists already in head
+function check_head(alink){
+  element = document.getElementsByTagName('link')
+  for(i=0; i<element.length; i++){
+      existing_link = element[i].href
+      //console.log(existing_link)
+      if(existing_link == alink){
+        return true
+      }
+  };
+}
+
+
+//  Append font names to head------
+
+function append_font_names(search=false, searchlist=null){
 
   max = font_names.length
   amount = current + load_card_amount
   i = current
 
-
+  if(search == false && searchlist == null){
   google_href="https://fonts.googleapis.com/css?family="
   for(i; i<max && i<amount; i++){
        font_name = font_names[i]
        google_link = google_href + font_name
-       var newlink = document.createElement('link');
-       newlink.rel = 'stylesheet';
-       newlink.href = google_link
-       document.head.appendChild(newlink);
-     }
+       exists = check_head(google_link)
+       if(!exists){
+         var newlink = document.createElement('link');
+         newlink.rel = 'stylesheet';
+         newlink.href = google_link
+         document.head.appendChild(newlink);
+       }
+     };
+   }
+   else{
+     google_href="https://fonts.googleapis.com/css?family="
+     for(i; i<max && i<amount; i++){
+          font_name = searchlist[i]
+          google_link = google_href + font_name
+          exists = check_head(google_link)
+          if(!exists){
+            var newlink = document.createElement('link');
+            newlink.rel = 'stylesheet';
+            newlink.href = google_link
+            document.head.appendChild(newlink);
+          }
+        };
+      }
 }
 //----------  add font to card, head ----//
 
 
-function create_cards(){
+function create_cards(search=false, searchlist=null){
   var max = font_names.length - 1;
   i = current
   amount = current + load_card_amount
-  for(i; i<max && i< amount; i++){
-     var fontName = font_names[i];
+  if(search == false && searchlist == null){
+    for(i; i<max && i< amount; i++){
+       var fontName = font_names[i];
 
-     var newNode = document.createElement('div');
-     var newH2 = document.createElement('h2');
-     var newH3 = document.createElement('h3');
-     newH2.innerHTML = fontName;
-     newH2.style.fontFamily = fontName;
-     newH3.className = 'sometext';
-     newH3.id = 'sometext';
-     newH3.style.fontFamily = fontName;
-     newH3.innerHTML = '"When you reach the end of your rope, tie a knot in it and hang on." -Franklin D. Roosevelt';
-     newNode.className = 'font_card';
-     //newNode.style.fontFamily = fontName;
+       var newNode = document.createElement('div');
+       var newH2 = document.createElement('h2');
+       var newH3 = document.createElement('h3');
+       newH2.innerHTML = fontName;
+       newH2.style.fontFamily = fontName;
+       newH3.className = 'sometext';
+       newH3.id = 'sometext';
+       newH3.style.fontFamily = fontName;
+       newH3.innerHTML = '"When you reach the end of your rope, tie a knot in it and hang on." -Franklin D. Roosevelt';
+       newNode.className = 'font_card';
+       //newNode.style.fontFamily = fontName;
 
-     newNode.appendChild(newH2)
-     newNode.appendChild(newH3)
-     document.getElementById('FontDeck').appendChild(newNode);
+       newNode.appendChild(newH2)
+       newNode.appendChild(newH3)
+       document.getElementById('FontDeck').appendChild(newNode);
+    }
+  }
+  else{
+
+    for(i; i<max && i< amount; i++){
+       var fontName = searchlist[i];
+
+       var newNode = document.createElement('div');
+       var newH2 = document.createElement('h2');
+       var newH3 = document.createElement('h3');
+       newH2.innerHTML = fontName;
+       newH2.style.fontFamily = fontName;
+       newH3.className = 'sometext';
+       newH3.id = 'sometext';
+       newH3.style.fontFamily = fontName;
+       newH3.innerHTML = '"When you reach the end of your rope, tie a knot in it and hang on." -Franklin D. Roosevelt';
+       newNode.className = 'font_card';
+       //newNode.style.fontFamily = fontName;
+
+       newNode.appendChild(newH2)
+       newNode.appendChild(newH3)
+       document.getElementById('FontDeck').appendChild(newNode);
+    }
+
   }
 }
 //----------  reset the page without reloading -----
@@ -271,11 +521,8 @@ function set_listeners() {
     dark_theme_check.addEventListener('click', get_and_set_checks);
     light_theme_check.addEventListener('click', get_and_set_checks);
     reset.addEventListener('click', reset_page_items);
-
-
-
-
 }
+
 
 function assign_json(myJson) {
   var data = myJson
@@ -305,8 +552,5 @@ window.onload = function () {
     set_DOM()
     get_fonts()
     set_listeners()
-
-
-
 
 }
